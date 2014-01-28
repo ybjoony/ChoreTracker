2b3cc9791f58a1759f3780f3d7ede18f39db9352
vagrant <vagrant@ybjoony-272VM>
2014-01-28 06:53:08 +0000

    added validation and relationship to chore

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,7 @@
 class Child < ActiveRecord::Base
   attr_accessible :active, :first_name, :last_name
+  has_many :chores
+  validates_presence_of :first_name
+  validates_presence_of :last_name
   
 end
