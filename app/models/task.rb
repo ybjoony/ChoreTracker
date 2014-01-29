82e2f488b59e2d8ecc903ea2ef7cc3422b8c4589
vagrant <vagrant@ybjoony-272VM>
2014-01-29 05:08:44 +0000

    added task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ActiveRecord::Base
   attr_accessible :active, :name, :points
-  
+  scope :alphabetical, order('name')
+  scope :active, where('active = ?', true)
+  has_many :chores
+  validates_presence_of :name
+  validates_numericality_of :points , :only_integer => true, :greater_than_or_equal_to => 0
 end
